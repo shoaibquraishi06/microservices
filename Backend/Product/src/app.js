64685adc910e2f113109dee0e0a0ea2d9dab4cfe
shoaibquraishi06 <dxshoaib51@gmail.com>
2026-04-26 22:18:47 +0530

    Fix CORS origin URL by adding trailing slash

--- Backend/Product/src/app.js
+++ Backend/Product/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://microservices-3-777q.onrender.com",
+    origin: "https://microservices-3-777q.onrender.com/",
     credentials: true
 }));
 app.get('/', (req, res) => {
