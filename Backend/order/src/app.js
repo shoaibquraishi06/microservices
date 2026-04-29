19ebe467de996ddf77c94c3dd324fb3a7cf96f45
shoaibquraishi06 <dxshoaib51@gmail.com>
2026-04-29 11:15:09 +0530

    Refactor CORS configuration: define options in a dedicated object for improved clarity

--- Backend/order/src/app.js
+++ Backend/order/src/app.js
@@ -1,29 +1,30 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const orderRoutes = require("./routes/order.routes")
-const cors = require('cors')
-
-
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const orderRoutes = require("./routes/order.routes");
+const cors = require("cors");
 
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+
+const corsOptions = {
   origin: [
     "http://localhost:5173",
     "https://nike-jordan-shoe.netlify.app"
   ],
   credentials: true
-}));
-app.options("*", cors());
+};
 
+app.use(cors(corsOptions));
+app.options("/*", cors(corsOptions));
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: "Order service is running"
-    });
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Order service is running"
+  });
+});
 
-app.use("/api/orders", orderRoutes)
+app.use("/api/orders", orderRoutes);
 
 module.exports = app;
