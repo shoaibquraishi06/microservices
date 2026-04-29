d1ff043f7350ec55894af71cf757418fdc59c3d0
shoaibquraishi06 <dxshoaib51@gmail.com>
2026-04-29 10:45:07 +0530

    Refactor CORS configuration: simplify options and improve readability

--- Backend/order/src/app.js
+++ Backend/order/src/app.js
@@ -1,38 +1,28 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const orderRoutes = require("./routes/order.routes");
-const cors = require("cors");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const orderRoutes = require("./routes/order.routes")
+const cors = require('cors')
+
+
 
 const app = express();
-
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://nike-jordan-shoe.netlify.app"
+  ],
+  credentials: true
+}));
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://nike-jordan-shoe.netlify.app"
-];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS blocked"));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-};
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Order service is running"
+    });
+})
 
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // IMPORTANT
-
-app.get("/", (req, res) => {
-  res.json({ message: "Order service is running" });
-});
-
-app.use("/api/orders", orderRoutes);
+app.use("/api/orders", orderRoutes)
 
 module.exports = app;
