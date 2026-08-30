24e7c7f5a666c54559afb9acc057c51db497e06d
shoaibquraishi06 <dxshoaib51@gmail.com>
2026-08-30 22:53:56 +0530

    feat: update ProductCard component with like functionality and enhance styling for buttons fix: update MONGO_URI in .env file for correct database connection style: adjust Navbar and product styles for better responsiveness and layout

--- Frontend/src/components/ProductCard.jsx
+++ Frontend/src/components/ProductCard.jsx
@@ -1,13 +1,20 @@
 
 import { useDispatch } from "react-redux";
+import { useState } from "react";
 import { addToCart } from "../feature/cartThunk";
 import Logo from "../assets/logo.jpg";
 import { FiShoppingBag } from "react-icons/fi";
+import { BiCartAlt } from "react-icons/bi";
+import { FaRegHeart } from "react-icons/fa";
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'; 
+
 import "../style/product.css";
 // import "../style/productCard.css"
 
 export default function ProductCard({ product }) {
   const dispatch = useDispatch(); 
+
+  const [ liked, setLiked ] = useState(false);
 
   if (!product) return null;
 
@@ -15,12 +22,50 @@ export default function ProductCard({ product }) {
     //  console.log("ADD TO CART:", product._id, product.title, product.price, product.images);
   dispatch(addToCart({ productId: product._id }));
   };
+   // Toggle function
+ 
+   const handleLikeToggle = () => {
+    setLiked(!liked);
+  };
+
+
+   
 
   return (
     // <ProductCard product={product} />;
     <div className="product-card">
+
+
+   <div className="whistlist-products">
+   <button 
+      onClick={handleLikeToggle}
+      style={{
+        position: 'absolute',
+        top: '20%',
+        right: '5%',
+        background: 'none',
+        border: 'none',
+        cursor: 'pointer',
+        fontSize: '1.5rem', // Adjust size of the icon
+       
+      }}
+      aria-label={liked ? "Unlike" : "Like"}
+    >
+      {liked ? (
+        <AiFillHeart style={{ color: '#c70d0d' }} /> // Filled red heart
+      ) : (
+        <AiOutlineHeart style={{ color: '#4B5563' }} /> // Outlined gray heart
+      )}
+    </button>
+    
+
+    
+      
+     </div>
+     
       <div className="logo">
-        <img src={Logo} alt="Nike" />
+
+        {/* <img src={Logo} alt="Nike" /> */}
       </div>
       <div className="product-img">
         <img
@@ -36,9 +81,19 @@ export default function ProductCard({ product }) {
  </div>
 
  <p>{product.description}</p>
+      
+      <div className="btn-section">
+      
         <button className="addToCart" onClick={handleAddToCart}>
         <span><FiShoppingBag /></span> <span> Add to Cart</span>
         </button>
+        
+       <button className="buyNow" >
+        <span><BiCartAlt /></span> <span> Buy Now</span>
+
+       </button>
+        
+        </div>
      
     </div>
   );
